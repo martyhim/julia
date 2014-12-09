487666f1c45819e98be72db727f61e5a142e0f16
Rafael Fourquet <fourquet.rafael@gmail.com>
2014-12-09 18:54:43 +0530

    implement RandomDevice RNG

--- base/dSFMT.jl
+++ base/dSFMT.jl
@@ -67,9 +67,11 @@ end
 ## Windows entropy
 
 @windows_only begin
-    function win32_SystemFunction036!(a::Array{UInt32})
-        ccall((:SystemFunction036,:Advapi32),stdcall,UInt8,(Ptr{Void},UInt32),a,length(a)*sizeof(eltype(a)))
+    function win32_SystemFunction036!{T}(a::Ptr{T}, nbytes::Int)
+        ccall((:SystemFunction036,:Advapi32),stdcall,UInt8,(Ptr{Void},UInt32),a,nbytes)
     end
+    win32_SystemFunction036!{T}(a::Array{T}) = win32_SystemFunction036!(pointer(a), length(a)*sizeof(eltype(a)))
+
 end
 
 end # module
